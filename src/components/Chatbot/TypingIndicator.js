b7b6a7666dc400e1fd3a53521e6de63e56a4c826
Jp Duarte <joao21412@gmail.com>
2024-11-13 19:16:50 -0300

    Updates

--- src/components/Chatbot/TypingIndicator.js
+++
@@ -1,9 +0,0 @@
-// frontend/src/components/Chatbot/TypingIndicator.js
-
-import React from 'react';
-
-const TypingIndicator = () => {
-  return <div>Chatbot está digitando...</div>;
-};
-
-export default TypingIndicator;
--- src/components/TestComponent.js
+++
@@ -1,8 +0,0 @@
-// src/components/TestComponent.js
-import React from 'react';
-
-const TestComponent = () => {
-  return <div>Teste de Componentes</div>;
-};
-
-export default TestComponent; // Exportação Padrão
